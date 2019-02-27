af100232aa4dc5928647c5b974074889a1a9461c
Jose <reyescortesjosemaria@gmail.com>
2019-02-26 23:18:27 -0600

    Primera parte (base de datos)

--- app/src/main/java/mx/martinezfernando/reyesjose/cecyt9/edu/ipn/memorama/MainActivity.java
+++ app/src/main/java/mx/martinezfernando/reyesjose/cecyt9/edu/ipn/memorama/MainActivity.java
@@ -6,20 +6,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     Button back;
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+    String tiempo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         back = findViewById(R.id.back);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("puntaje");
     }
 
     public void regreso(View v) {
         Intent i = new Intent(this, Menu.class);
         finish();
         startActivity(i);
+    }
+
+    public void getTime(View v) {
+        tiempo = "01:15";
+
+        myRef.push().setValue(tiempo);
     }
 }
--- app/src/main/java/mx/martinezfernando/reyesjose/cecyt9/edu/ipn/memorama/Puntajes.java
+++ app/src/main/java/mx/martinezfernando/reyesjose/cecyt9/edu/ipn/memorama/Puntajes.java
@@ -3,14 +3,19 @@ package mx.martinezfernando.reyesjose.cecyt9.edu.ipn.memorama;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 public class Puntajes extends AppCompatActivity {
+
+    RecyclerView rec;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_puntajes);
+        rec = findViewById(R.id.datos);
     }
 
     public void regreso(View v) {
