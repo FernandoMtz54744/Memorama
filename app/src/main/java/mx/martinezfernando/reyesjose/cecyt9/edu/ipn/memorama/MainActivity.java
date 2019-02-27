8c7ec8cfb2bf81a6002e697e9247fb836a651789
Jose <reyescortesjosemaria@gmail.com>
2019-02-26 20:31:05 -0600

    Primera actualizacion

--- app/src/main/java/mx/martinezfernando/reyesjose/cecyt9/edu/ipn/memorama/MainActivity.java
+++ app/src/main/java/mx/martinezfernando/reyesjose/cecyt9/edu/ipn/memorama/MainActivity.java
@@ -1,13 +1,25 @@
 package mx.martinezfernando.reyesjose.cecyt9.edu.ipn.memorama;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        back = findViewById(R.id.back);
+    }
+
+    public void regreso(View v) {
+        Intent i = new Intent(this, Menu.class);
+        finish();
+        startActivity(i);
     }
 }
